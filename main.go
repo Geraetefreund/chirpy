bb4e9f68780a311198c42db0e51aca4a16692573
Geraetefreund <friteuse@arcor.de>
2025-10-26 10:04:38 +0000

    ch2:l1 stateful handlers done

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -17,17 +18,32 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Metrics reset"))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	metrics := apiConfig{}
 
 	myServeMux := http.NewServeMux()
-	myServeMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	myServeMux.Handle("/app/", metrics.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 	myServeMux.Handle("/app/assets/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
 	myServeMux.HandleFunc("/healthz/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	myServeMux.HandleFunc("/metrics/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		// w.WriteHeader(http.StatusOK) // obsolete, automatically when writing to body
+		fmt.Fprintf(w, "Hits: %d", metrics.fileserverHits.Load())
+	})
+
+	myServeMux.HandleFunc("/reset/", func(w http.ResponseWriter, req *http.Request) {
+		metrics.resetMetrics(w, req)
 	})
 
 	srv := &http.Server{
