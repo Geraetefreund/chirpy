a239a63298a33796d8548326ab3617f0643f73e0
Geraetefreund <friteuse@arcor.de>
2025-10-27 09:49:39 +0000

    CH4L6-done

--- handler_validate.go
+++ handler_validate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,7 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -26,8 +27,32 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
+	/*
+		we could use a nested loop (simple, easy to read) but O(n) or a map for O(1)
+		normally, if profanities is data or too large, we create the map from a slice
+		profanities := []string{"kerfuffle", "sharbert", "fornax"}
+		profanityMap := make(map[string]struct{})
+		for _, p := range profanities {
+			profanityMap[p] = struct{}{}
+		}
+
+		below, I simply created the map right there since it was only three words.
+	*/
+
+	profanityMap := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	words := strings.Fields(params.Body)
+	for i, w := range words {
+		if _, ok := profanityMap[strings.ToLower(w)]; ok {
+			words[i] = "****"
+		}
+	}
+	cleanedBody := strings.Join(words, " ")
 
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanedBody,
 	})
 }
