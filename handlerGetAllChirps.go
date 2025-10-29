486e8676b298c7a34f0b274411b2264ce54c719e
Geraetefreund <friteuse@arcor.de>
2025-10-29 13:00:08 +0000

    Ch6L9-done

---
+++ handlerGetAllChirps.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, chirps)
+}
--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUserInfo)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetAllChirps)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerTruncateUsersChirps)
